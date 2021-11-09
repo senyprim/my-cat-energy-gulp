623aef7f4b6777e032104c08a63181d6acc46a01
seny <seny_ussur@mail.ru>
2021-11-09 17:51:42 +1000

    init

---
+++ gulpfile.js
@@ -0,0 +1,214 @@
+const ALL_FILE = '*.*';
+const BITMAP_FILE = ["png", "jpg", "gif"];
+const SVG_FILE = ["svg"]
+const bitmapImg = `*.{${BITMAP_FILE.join(',')}}`;
+const svgImg = `*.{${SVG_FILE.join(',')}}`;
+const SPRITE_FILES = ['sprite-*.svg'];
+
+const pathDir = require('path');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));
+const htmlmin = require("gulp-htmlmin");
+const posthtml = require("gulp-posthtml");
+const include = require("posthtml-include");
+const svgsprite = require("gulp-svg-sprite");
+const browserSync = require("browser-sync");
+const prefixer = require("gulp-autoprefixer");
+const cssmin = require("gulp-clean-css");
+const concat = require("gulp-concat");
+const rimraf = require("rimraf");
+const imagemin = require("gulp-imagemin");
+const woff = require("gulp-ttf2woff");
+const woff2 = require("gulp-ttf2woff2");
+
+
+const scriptsLib = [
+    "./node_modules/slick-slider/slick/slick.min.js",
+];
+const stilesLib = [
+    "./node_modules/normalize.css/normalize.css",
+    "./node_modules/slick-slider/slick/slick.css",
+];
+
+
+const path = {
+    build: {
+        //Тут мы укажем куда складывать готовые после сборки файлы
+        html: "build/",
+        js: "build/js/",
+        css: "build/css/",
+        img: "build/img/",
+        fonts: "build/fonts/",
+    },
+    src: {
+        //Пути откуда брать исходники
+        html: "src/*.html", //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: "src/js/*.js", //В стилях и скриптах нам понадобятся только main файлы
+        style: "src/sass/style.scss",
+        img: "src/img/**/", //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        img_min: "src/img-min/", //Каталог минифицированных изображений - чтобы не надобыло минифицировать постоянно
+        fonts: "src/fonts/",
+    },
+    watch: {
+        //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        src: "src/",
+        build: "build/",
+        html: "src/*.html",
+        js: "src/js/**/*.js",
+        style: "src/sass/**/*.scss",
+        img: "src/img/**/*.*",
+        img_min: "src/img-min/**/*.*",
+        fonts: "src/fonts/**/*.*",
+    },
+    clean: ["./build", "./src/img-min"]
+};
+//Удаление папок
+const clean = (cb) => {
+        return rimraf(`{${path.clean.join(',')}}`, cb);
+    }
+    //Сервер
+const server = (done) => {
+    browserSync.init({
+        server: {
+            baseDir: path.build.html
+        },
+        cors: true,
+        notify: false,
+        ui: false,
+    });
+    done();
+}
+const reload = (cb) => {
+    browserSync.reload();
+    cb();
+}
+exports.server = server;
+exports.reload = reload;
+const ttfToWoff = () => { //TODO
+    return gulp
+        .src("src/fonts/**/*.ttf")
+        .pipe(woff())
+        .pipe(gulp.dest(path.src.fonts));
+}
+const ttfToWoff2 = () => { //TODO
+    return gulp
+        .src('src/fonts/**/*.ttf')
+        .pipe(woff2())
+        .pipe(gulp.dest(path.src.fonts));
+}
+const copyFonts = () => {
+    return gulp
+        .src("src/fonts/**/*.*")
+        .pipe(gulp.dest(path.build.fonts));
+}
+const fonts = gulp.series(gulp.parallel(ttfToWoff, ttfToWoff2), copyFonts);
+
+exports.fonts = fonts;
+
+
+// HTML
+const html = () => {
+    return gulp.src(path.src.html, { allowEmpty: true })
+        .pipe(plumber())
+        .pipe(posthtml([include()])) //Проверим include  в html
+        .pipe(sourcemaps.init())
+        .pipe(htmlmin({
+            minifyJS: true,
+            minifyURLs: true,
+            collapseWhitespace: false,
+            removeComments: true,
+            sortAttributes: true,
+            sortClassName: true
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+}
+const stylesLib = () => {
+    return gulp
+        .src(stilesLib, { allowEmpty: true })
+        .pipe(plumber())
+        .pipe(concat("lib.css"))
+        .pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(cssmin()) //Сожмем
+        .pipe(gulp.dest(path.build.css)); //И в build
+}
+const style = () => {
+    return (
+        gulp
+        .src(path.src.style, { allowEmpty: true }) //Выберем наш главный стиль
+        .pipe(plumber())
+        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(sass()) //Скомпилируем
+        .pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(cssmin()) //Сожмем
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css)) //И в build
+    );
+}
+exports.style = style;
+
+const bitMapImage = () => {
+    return gulp
+        .src(path.src.img + '*.*')
+
+    //.src(path.src.img + '/*.jpg', { allowEmpty: true, since: gulp.lastRun(bitMapImage) }) //Выберем каталог изображений измененных с последнего раза
+    .pipe(
+            imagemin([
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 60, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 3 }),
+            ])
+        )
+        .pipe(gulp.dest(path.build.img)); //И бросим в минифицированные
+}
+exports.bitMapImage = bitMapImage;
+const svgImage = () => {
+    return gulp
+        .src(path.src.img + svgImg, `!${SPRITE_FILES}`, { allowEmpty: true }) //Выберем каталог svg изображений
+        .pipe(imagemin([imagemin.svgo()]))
+        .pipe(gulp.dest(path.build.img)); //И бросим в минифицированные
+}
+const svgSprite = (name) => {
+    const config = {
+        mode: {
+            stack: {
+                sprite: `${path.build.img}/sprite-${name}.svg`,
+            }
+        }
+    };
+    return gulp
+        .src(path.src.img + SPRITE_FILES, { allowEmpty: true }) //Выберем каталог svg изображений
+        .pipe(svgsprite(config))
+        .pipe(gulp.dest(path.src.img))
+        .pipe(gulp.dest(path.build.img));
+
+}
+
+const watcher = () => {
+    gulp.watch(path.watch.html, gulp.series(html, reload));
+    gulp.watch(path.watch.img, gulp.series(gulp.parallel(bitMapImage, svgImage, svgSprite)), reload);
+    gulp.watch(path.watch.style, gulp.series(style, reload));
+}
+
+
+const build = gulp.series(
+    clean,
+    gulp.parallel(
+        fonts,
+        style,
+        stylesLib,
+        html,
+        bitMapImage,
+        svgImage,
+        svgSprite,
+    )
+);
+
+exports.build = build;
+exports.default = gulp.series(
+    build,
+    server,
+    watcher
+);
